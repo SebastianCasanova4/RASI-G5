7aabad0af789316558c84d9c8fa44ddb664e4793
tu usuario github <j.mejia17@uniandes.edu.co>
2023-10-18 15:06:58 -0500

    login

--- RASI/RASI_app/urls.py
+++ RASI/RASI_app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path,include
 from .views import home, login
 urlpatterns = [
     path('', home, name='home'),
-    path('login/', login, name='login')
+    path('login/', login, name='login'),
+    
 ]
