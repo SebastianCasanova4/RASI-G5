5c94d5ebb432d9db872134a0d4d4a1b86e546207
Julio Peña Tovar <ja.penat@uniandes.edu.co>
2023-10-21 00:17:27 -0500

    cambios a urls y views para que funcione el load balancer

--- RASI/RASI_app/urls.py
+++ RASI/RASI_app/urls.py
@@ -1,11 +1,11 @@
-from django import views
+
 from django.urls import path,include
-from .views import home, registro
+from .views import home, registro, healthCheck
 from django.conf import settings
 urlpatterns = [
     path('', home, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('registro/', registro, name='registro'),
-    path('health-check/', views.healthCheck)
+    path('health-check/', healthCheck)
 ]
 
