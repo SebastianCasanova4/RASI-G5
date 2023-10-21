f1dd67fc327b021d3063ad169a71fff1b677b107
Julio Peña Tovar <ja.penat@uniandes.edu.co>
2023-10-20 21:29:08 -0500

    el proyecto ahora se conecta las db de postgres sql

--- RASI/RASI/settings.py
+++ RASI/RASI/settings.py
@@ -81,14 +81,15 @@ WSGI_APPLICATION = 'RASI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USERNAME'),
-        'PASSWORD': os.environ.get('DB_USERNAME_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get(''),
+        'NAME': 'grupo5_db',
+        'USER': 'grupo5_user',
+        'PASSWORD': 'grupo5',
+        'HOST': '10.58.240.3',
+        'PORT': '',
     }
 }
 
