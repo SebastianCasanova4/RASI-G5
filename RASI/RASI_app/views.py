3384994d8e1515057147b9ca9dec3c6035fdfe29
Julio Peña Tovar <ja.penat@uniandes.edu.co>
2023-12-06 06:01:11 -0500

    cambio

--- RASI/RASI_app/views.py
+++ RASI/RASI_app/views.py
@@ -1,26 +1,16 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from .forms import CustomUserCreationForm
-from django.contrib.auth import authenticate, login
-from django.http import JsonResponse
-from pymongo import MongoClient
-import datetime
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.parsers import JSONParser
-from django.conf import settings
-from bson.objectid import ObjectId
-
-# Create your views here.
-
-def home(request):
-    return render(request, 'RASI_app/home.html')
-
-from django.contrib import messages
-
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from pymongo import MongoClient
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from django.conf import settings
+from bson.objectid import ObjectId
+
+def home(request):
+    return render(request, 'RASI_app/home.html')
 
 def registro(request):
     data = {
@@ -31,7 +21,7 @@ def registro(request):
         formulario = CustomUserCreationForm(request.POST)
         if formulario.is_valid():
             formulario.save()
-            user = authenticate(username=formulario.cleaned_data["username"], password=formulario.cleaned_>
+            user = authenticate(username=formulario.cleaned_data["username"], password=formulario.cleaned_data["password1"])
             login(request, user)
             messages.success(request, "Te has registrado correctamente")
             return redirect(to="home")
@@ -40,14 +30,14 @@ def registro(request):
     return render(request, 'registration/registro.html', data)
 
 def healthCheck(request):
-    
     return HttpResponse('ok')
 
-    @api_view(["GET", "POST"])
+@api_view(["GET", "POST"])
 def cedulas(request):
     client = MongoClient(settings.MONGO_CLI)
     db = client.rasi_db
     cedulas = db['cedulas']
+    
     if request.method == "GET":
         result = []
         data = cedulas.find({})
@@ -61,27 +51,27 @@ def cedulas(request):
         client.close()
         return JsonResponse(result, safe=False)
     
-    if request.method == 'POST':
+    elif request.method == 'POST':
         data = JSONParser().parse(request)
         result = cedulas.insert(data)
-        respo ={
+        respo = {
             "MongoObjectID": str(result),
             "Message": "nuevo objeto en la base de datos"
         }
         client.close()
         return JsonResponse(respo, safe=False)
 
-
 @api_view(["GET", "POST"])
 def cedulasDetail(request, pk):
     client = MongoClient(settings.MONGO_CLI)
     db = client.rasi_db
-    cedulas  = db['cedulas']
+    cedulas = db['cedulas']
+    
     if request.method == "GET":
         data = cedulas.find({'cedula': pk})
         result = []
         for dto in data:
-            jsonData ={
+            jsonData = {
                 'id': str(dto['_id']),
                 "cedula": dto['cedula'],
                 'sedes': dto['sedes']
@@ -89,13 +79,14 @@ def cedulasDetail(request, pk):
             result.append(jsonData)
         client.close()
         return JsonResponse(result[0], safe=False)
-    if request.method == "POST":
+    
+    elif request.method == "POST":
         data = JSONParser().parse(request)
-        result = variables.update(
+        result = cedulas.update(
             {'cedula': pk},
             {'$push': {'sedes': data}}
         )
-        respo ={
+        respo = {
             "MongoObjectID": str(result),
             "Message": "nuevo objeto en la base de datos"
         }
